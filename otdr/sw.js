0427331c7954bafaded5f821911282a30709ef73
tonycamposjr <163766502+tonycamposjr@users.noreply.github.com>
2026-09-25 02:35:51 -0300

    Publica Leitor OTDR V2 como PWA

---
+++ otdr/sw.js
@@ -0,0 +1,10 @@
+const CACHE='otdr-v2-site-1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>cached)));
+});
